57e352d37c834f195775485dd8f215737269d242
chanteneviljoen <chantene.viljoen@my.jcu.edu.au>
2021-10-07 23:35:40 +1000

    Add taxi_simulator.py to prac_08

---
+++ prac_08/taxi_simulator.py
@@ -0,0 +1,62 @@
+"""
+CP1404/CP5632 Practical
+Taxi simulator
+"""
+from prac_06.car import Car
+from prac_08.taxi import Taxi
+from prac_08.silver_service_taxi import SilverServiceTaxi
+
+MENU = "q)uit, c)hoose taxi, d)rive"
+
+
+def main():
+    """Taxi simulator program."""
+    total_bill = 0
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2),
+             SilverServiceTaxi("Hummer", 200, 4)]
+    current_taxi = None
+
+    print("Let's drive!")
+    print(MENU)
+
+    menu_choice = input(">>> ").lower()
+    while menu_choice != "q":
+        if menu_choice == "c":
+            print("Taxis available: ")
+            display_taxis(taxis)
+            taxi_choice = int(input("Choose taxi: "))
+            try:
+                current_taxi = taxis[taxi_choice]
+            except IndexError:
+                print("Invalid taxi choice")
+
+        elif menu_choice == "d":
+            if current_taxi:
+                distance_to_drive = float(input("Drive how far? "))
+                current_taxi.start_fare()
+                current_taxi.drive(distance_to_drive)
+                trip_cost = current_taxi.get_fare()
+                print("Your {} trip cost you ${:.2f}".format(current_taxi.name,
+                                                             trip_cost))
+                total_bill += trip_cost
+            else:
+                print("You need to choose a taxi before you can drive")
+        else:
+            print("Invalid option")
+
+        print("Bill to date: ${:.2f}".format(total_bill))
+        print(MENU)
+        menu_choice = input(">>> ").lower()
+
+    print("Total trip cost: ${:.2f}".format(total_bill))
+    print("Taxis are now:")
+    display_taxis(taxis)
+
+
+def display_taxis(taxis):
+    """Display numbered list of taxis."""
+    for i, taxi in enumerate(taxis):
+        print("{} - {}".format(i, taxi))
+
+
+main()

191fc8ee07dfcd5edced6366265acfbd72d1d695
chanteneviljoen <chantene.viljoen@my.jcu.edu.au>
2021-10-26 20:01:47 +1000

    Add updates to wiki.py

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,7 +1,7 @@
 import wikipedia
 
 
-user_search_input = input('Enter page title / search phrase: ')
+user_search_input = input('Enter a search phrase: ')
 while user_search_input != '':
     try:
         page = wikipedia.page(user_search_input)
@@ -9,9 +9,9 @@ while user_search_input != '':
         print(page.summary)
         print(page.url)
     except wikipedia.exceptions.DisambiguationError as e:
-        print('Please enter a more specific search phrase, such as:')
+        print('Please be more specific, like:')
         for option in e.options[:5]:
             print(option)
     except wikipedia.exceptions.PageError:
         print('Page does not exist')
-    user_search_input = input('Enter page title / search phrase: ')
+    user_search_input = input('Enter a search phrase: ')

7eeeb2f298e667f1ce74cda5ea998ff7789a2d20
chanteneviljoen <chantene.viljoen@my.jcu.edu.au>
2021-10-26 19:45:28 +1000

    Add changes to complete TODO comments

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -22,7 +22,7 @@ def is_long_word(word, length=5):
     >>> is_long_word("Python", 6)
     True
     """
-    return len(word) > length
+    return len(word) >= length
 
 
 def run_tests():
@@ -32,7 +32,7 @@ def run_tests():
     # the test below should fail
     assert repeat_string("hi", 2) == "hi hi"
 
-    # TODO: 1. fix the repeat_string function above so that it passes the failing test
+    # 1. fix the repeat_string function above so that it passes the failing test
     # Hint: "-".join(["yo", "yo"] -> "yo-yo"
 
     # assert test with custom message,
@@ -41,20 +41,24 @@ def run_tests():
     test_car = Car()
     assert test_car.odometer == 0, "Car does not set odometer correctly"
 
-    # TODO: 2. write assert statements to show if Car sets the fuel correctly
+    # 2. write assert statements to show if Car sets the fuel correctly
     # Note that Car's __init__ function sets the fuel in one of two ways:
     # using the value passed in or the default
     # You should test both of these
     test_car = Car(fuel=10)
+    assert test_car.fuel == 10
+
+    test_car = Car()
+    assert test_car.fuel == 0
 
 
-run_tests()
+# run_tests()
 
-# TODO: 3. Uncomment the following line and run the doctests
+# 3. Uncomment the following line and run the doctests
 # (PyCharm may see your >>> doctest comments and run doctests anyway.)
 # doctest.testmod()
 
-# TODO: 4. Fix the failing is_long_word function
+# 4. Fix the failing is_long_word function
 # (don't change the tests, change the function!)
 
 # TODO: 5. Write and test a function to format a phrase as a sentence,
@@ -66,3 +70,25 @@ run_tests()
 # and one more you decide (one that is valid!)
 # test this and watch the tests fail
 # then write the body of the function so that the tests pass
+
+def format_phrase_to_sentence(phrase):
+    """
+    Format a phrase as a sentence, starting with a capital and ending with a .
+    >>> format_phrase_to_sentence('hello')
+    'Hello.'
+    >>> format_phrase_to_sentence('It is an ex parrot.')
+    'It is an ex parrot.'
+    >>> format_phrase_to_sentence('This subject rocks')
+    'This subject rocks.'
+    """
+    # capitalise sentence
+    sentence = phrase.capitalize()
+    # add the full stop
+    if sentence[-1] != '.':
+        sentence += '.'
+    return sentence
+
+
+run_tests()
+
+doctest.testmod()
